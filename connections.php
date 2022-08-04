64b506e388b6b9c7ba03fad26f71127f41da6e22
sakss-g <sakshi.gupta1214@gmail.com>
2022-08-04 13:21:16 +0545

    db connection

--- connections.php
+++ connections.php
@@ -1,13 +1,13 @@
 <?php
 
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "";
-$dbname = "evit";
+    $dbhost = "localhost";
+    $dbuser = "root";
+    $dbpass = "";
+    $dbname = "evit";
 
-if(!$con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname))
-{
-    die("failed to connect!");
-}
+    if(!$con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname))
+    {
+        die("failed to connect!");
+    }
 
 ?>
