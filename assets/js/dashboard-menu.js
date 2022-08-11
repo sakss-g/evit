a3640ef558ad2302aa26c65c286f16f90db2269c
sakss-g <sakshi.gupta1214@gmail.com>
2022-08-11 13:42:14 +0545

    dashboard

--- assets/js/dashboard-menu.js
+++ assets/js/dashboard-menu.js
@@ -1,33 +1,11 @@
-jQuery(document).on('click', '.search-wrapper span.fa-bars', function()
-{
-    jQuery( this ).parent().toggleClass( 'visible' );
+jQuery( document ).ready( function(){
+    jQuery(document).on('click', '#bars', function(){    
+        //e.preventDefault();
+        jQuery( 'body' ).toggleClass('show-sidebar');
+    });
 
-    if( jQuery( this ).parent().hasClass( 'visible' ) )
-    {
-        $('#bars').removeClass('.sidebar').addClass('.sidebar-dash');
-        //$("sidebar.sidebar-menu").toggleClass("menu_show");
-    }
-
-});
-
-
-
-// $( document ).ready(function() {
-
-//     $( ".cross" ).hide();
-//     $( ".menu" ).hide();
-//     $( ".hamburger" ).click(function() {
-//         $( ".menu" ).slideToggle( "slow", function() {
-//             $( ".hamburger" ).hide();
-//             $( ".cross" ).show();
-//         });
-//     });
-    
-//     $( ".cross" ).click(function() {
-//         $( ".menu" ).slideToggle( "slow", function() {
-//             $( ".cross" ).hide();
-//             $( ".hamburger" ).show();
-//         });
-//     });
-    
-// });
+    jQuery(document).on('click', '.close', function(){    
+        //e.preventDefault();
+        jQuery( 'body' ).removeClass('show-sidebar');
+     });
+} );
